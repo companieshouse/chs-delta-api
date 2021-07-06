2eba80061e4cd86b5d305589b02f8c48d4d0fe62
mreis-ch <mreis@companieshouse.gov.uk>
2021-07-06 16:26:25 +0100

    Add officer delta endpoint
    Add a new endpoint /delta/officers to chs-delta-api. This is initial fair weather path and will return status of 200
    Add handler for endpoint
    Add officerDelta model based on OpenApi spec
    Add unit tests
    
    DSND-48

---
+++ handlers/officersHandler.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/companieshouse/chs-delta-api/models"
+	"net/http"
+)
+
+// OfficerHandler offers a handler by which to publish a message onto a kafka topic.
+type OfficerHandler struct {
+}
+
+// NewOfficerHandler returns a OfficerHandler.
+func NewOfficerHandler() *OfficerHandler {
+	return &OfficerHandler{
+	}
+}
+
+// ServeHTTP accepts an incoming officer request via a POST method and validates and forwards to the XXX service.
+func (kp *OfficerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	var officersData models.OfficerDelta
+
+	// If the decoding fails, it will most likely be due to bad data being submitted by the user.
+	if err := json.NewDecoder(r.Body).Decode(&officersData) ; err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
---
+++ handlers/officersHandler_test.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"bytes"
+	. "github.com/smartystreets/goconvey/convey"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	requestBody = `{
+        "officers" : [
+			{
+				"company_number" : "99999999",
+				"company_name" : "Test company 1",
+				"forename" : "Test forename 1",
+				"middle_name" : "Test middle name 1",
+				"surname" : "Test surname 1",
+				"age" : 25
+			},
+			{
+				"company_number" : "55555555",
+				"company_name" : "Test company 2",
+				"forename" : "Test forename 2",
+				"middle_name" : "Test middle name 2",
+				"surname" : "Test surname 2",
+				"age" : 39
+			}
+    	],
+    	"created_at" : "01/01/2000",
+    	"delta_at" : "01/01/2000"
+	}`
+
+	badRequestBody = `{
+	"test" : "xeiuohfwnw8347",
+	"wrong" : "049301-458312-500968",
+	"input" : "00006400"
+	}`
+)
+
+func TestNewKafkaPublisherHandler(t *testing.T) {
+
+	Convey("Then the constructor returns me a valid OfficerHandler", t, func() {
+
+		officerHandler := NewOfficerHandler()
+
+		So(officerHandler, ShouldNotBeNil)
+	})
+}
+
+func TestKafkaPublisherHandlerWithCorrectRoute(t *testing.T) {
+	Convey("Given a HTTP request for /delta/officers", t, func() {
+
+		req := httptest.NewRequest("POST", "/delta/officers", bytes.NewBuffer([]byte(requestBody)))
+		resp := httptest.NewRecorder()
+
+		Convey("When the request is handled by the router", func() {
+
+			handler := NewOfficerHandler()
+			handler.ServeHTTP(resp, req)
+
+			Convey("Then the response should be 200", func() {
+				So(resp.Code, ShouldEqual, http.StatusOK)
+			})
+		})
+	})
+}
