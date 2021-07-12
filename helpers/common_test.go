24ee7f8a5f76cc102997138245b8d4506a8451cf
Faisal Amin <famin@companieshouse.gov.uk>
2021-07-12 14:40:36 +0100

    Add more unit tests to improve coverage

--- helpers/common_test.go
+++ helpers/common_test.go
@@ -2,15 +2,18 @@ package helpers
 
 import (
 	"bytes"
+	"errors"
 	. "github.com/smartystreets/goconvey/convey"
 	"io/ioutil"
 	"net/http"
 	"testing"
 )
 
-const requestExample = `{
+const (
+	requestExample = `{
 "test": "example"
 }`
+)
 
 func TestGetDataFromRequestSuccess(t *testing.T) {
 
@@ -27,3 +30,25 @@ func TestGetDataFromRequestSuccess(t *testing.T) {
 	})
 }
 
+//Used in error test for asserting error occurs
+type mockRequestError struct{}
+func (mockRequestError) Read(p []byte) (n int, err error) {
+	return 0, errors.New("test error")
+}
+
+func TestGetDataFromRequestError(t *testing.T) {
+
+	Convey("Given I pass a request into the GetDataFromRequest function", t, func() {
+
+		reqBody := http.Request{Body: ioutil.NopCloser(mockRequestError{}),
+		}
+
+		data, err := GetDataFromRequest(&reqBody)
+
+		Convey("Then I am given a an error", func() {
+
+			So(data, ShouldEqual, "")
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
