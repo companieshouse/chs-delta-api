abe339a87a11f69b592d4875a111996b96653d76
Faisal Amin <famin@companieshouse.gov.uk>
2021-07-20 11:44:51 +0100

    Add unit tests

---
+++ config/config_test.go
@@ -0,0 +1,30 @@
+package config
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"os"
+	"testing"
+)
+func TestGetGoFigureErrors(t *testing.T) {
+	Convey("When I try to get the config via the Get method", t, func(){
+		cfg, err := Get()
+		Convey("Then I am given an error when calling go figure and it is handled correctly", func() {
+			So(cfg, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
+func TestGetNoErrors(t *testing.T) {
+	_ = os.Setenv("BIND_ADDR", "bind_addr")
+	_ = os.Setenv("KAFKA_BROKER_ADDR", "kafka_broker_addr,kafka_broker_addr")
+	_ = os.Setenv("SCHEMA_REGISTRY_URL", "schema_registry_url")
+	_ = os.Setenv("OFFICER_DELTA_TOPIC", "officer_delta_topic")
+	_ = os.Setenv("OPEN_API_SPEC", "open_api_spec")
+	Convey("When I try to get the config via the Get method and all config vars are provided", t, func(){
+		cfg, err := Get()
+		Convey("Then I am given a correctly configured config", func() {
+			So(cfg, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+		})
+	})
+	os.Clearenv()
+}
