f57125a4aac2016953f17cb180b762f624f71220
iichr <ciliev@companieshouse.gov.uk>
2022-02-07 14:53:54 +0000

    case_number must now contain at least 1 item + all dates now limited to exactly 8 characters + extended unit test to cover date length validation

--- validation/schema_testing/insolvency/insolvencyDeltaSchema_test.go
+++ validation/schema_testing/insolvency/insolvencyDeltaSchema_test.go
@@ -11,15 +11,17 @@ import (
 )
 
 const (
-	requestBodiesLocation            = "./request_bodies/"
-	okRequestBodyLocation            = requestBodiesLocation + "ok_request_body"
-	typeErrorRequestBodyLocation     = requestBodiesLocation + "type_error_request_body"
-	requiredErrorRequestBodyLocation = requestBodiesLocation + "required_error_request_body"
+	requestBodiesLocation              = "./request_bodies/"
+	okRequestBodyLocation              = requestBodiesLocation + "ok_request_body"
+	typeErrorRequestBodyLocation       = requestBodiesLocation + "type_error_request_body"
+	requiredErrorRequestBodyLocation   = requestBodiesLocation + "required_error_request_body"
+	dateLengthErrorRequestBodyLocation = requestBodiesLocation + "date_length_error_request_body"
 
 	responseBodiesLocation                 = "./response_bodies/"
 	typeErrorResponseBodyLocation          = responseBodiesLocation + "type_error_response_body"
 	requiredErrorResponseBodyLocation      = responseBodiesLocation + "required_error_response_body"
 	noRequestBodyErrorResponseBodyLocation = responseBodiesLocation + "no_request_body_error_response_body"
+	dateLengthErrorResponseBodyLocation    = responseBodiesLocation + "date_length_error_response_body"
 
 	insolvencyEndpoint = "/delta/insolvency"
 	apiSpecLocation    = "../../../apispec/api-spec.yml"
@@ -79,7 +81,7 @@ func TestUnitInsolvencyDeltaSchemaTypeErrors(t *testing.T) {
 	})
 }
 
-// TestUnitInsolvencyDeltaSchemaRequiredErrors asserts that when an invalid request body is given with missing mandatory values.
+// TestUnitInsolvencyDeltaSchemaRequiredErrors asserts that when an invalid request body is given with missing mandatory values,
 // then an errors array is returned, stating that required values are missing.
 func TestUnitInsolvencyDeltaSchemaRequiredErrors(t *testing.T) {
 
@@ -99,6 +101,33 @@ func TestUnitInsolvencyDeltaSchemaRequiredErrors(t *testing.T) {
 			Convey("Then I am given an errors array response as validation errors have been found", func() {
 				mandatoryErrorsResponseBody := common.ReadRequestBody(requiredErrorResponseBodyLocation)
 				match := common.CompareActualToExpected(validationErrs, mandatoryErrorsResponseBody)
+
+				So(validationErrs, ShouldNotBeNil)
+				So(match, ShouldEqual, true)
+			})
+		})
+	})
+}
+
+// TestUnitInsolvencyDeltaDateLengthErrors asserts that when a request body is given with dates which are not of length 8
+// then an errors array is returned.
+// NOTE: there is no validation on the format of the dates in the spec, only properties asserted are type: string and [min|max]Length: 8
+func TestUnitInsolvencyDeltaDateLengthErrors(t *testing.T) {
+
+	Convey("Given I want to test the insolvency-delta API schema to assert mandatory validation is working correctly", t, func() {
+		dateErrorsRequestBody := common.ReadRequestBody(dateLengthErrorRequestBodyLocation)
+
+		r := httptest.NewRequest(methodPost, insolvencyEndpoint, bytes.NewBuffer(dateErrorsRequestBody))
+		r = common.SetHeaders(r)
+
+		Convey("When I call to validate the request body, providing an valid request with missing mandatory values", func() {
+			chv, _ := validation.NewCHValidator(apiSpecLocation)
+
+			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+
+			Convey("Then I am given an errors array response as validation errors have been found", func() {
+				dateErrorsResponseBody := common.ReadRequestBody(dateLengthErrorResponseBodyLocation)
+				match := common.CompareActualToExpected(validationErrs, dateErrorsResponseBody)
 
 				So(validationErrs, ShouldNotBeNil)
 				So(match, ShouldEqual, true)
