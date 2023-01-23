8407be08dfb454e17daee474e1e4f9ebf2ab5fcb
beldhoCH <beldho@companieshouse.gov.uk>
2023-01-23 10:46:20 +0000

    reformat indentation 2

--- validation/schema_testing/psc/pscStatementDeltaSchema_test.go
+++ validation/schema_testing/psc/pscStatementDeltaSchema_test.go
@@ -2,110 +2,110 @@ package psc
 
 import (
     "bytes"
-	"net/http/httptest"
-	"testing"
+    "net/http/httptest"
+    "testing"
 
-	"github.com/companieshouse/chs-delta-api/validation"
-	"github.com/companieshouse/chs-delta-api/validation/schema_testing/common"
-	. "github.com/smartystreets/goconvey/convey"
+    "github.com/companieshouse/chs-delta-api/validation"
+    "github.com/companieshouse/chs-delta-api/validation/schema_testing/common"
+    . "github.com/smartystreets/goconvey/convey"
 )
 
 const (
-    requestBodiesLocation            = "./request_bodies/"
-	okRequestBodyLocation            = requestBodiesLocation + "ok_request_body"
-	typeErrorRequestBodyLocation     = requestBodiesLocation + "type_error_request_body"
-	requiredErrorRequestBodyLocation = requestBodiesLocation + "required_error_request_body"
-	deleteRequestBodyLocation        = requestBodiesLocation + "delete_request_body"
+    requestBodiesLocation              = "./request_bodies/"
+    okRequestBodyLocation              = requestBodiesLocation + "ok_request_body"
+    typeErrorRequestBodyLocation       = requestBodiesLocation + "type_error_request_body"
+    requiredErrorRequestBodyLocation   = requestBodiesLocation + "required_error_request_body"
+    deleteRequestBodyLocation        = requestBodiesLocation + "delete_request_body"
 
-	responseBodiesLocation            = "./response_bodies/"
-	typeErrorResponseBodyLocation     = responseBodiesLocation + "type_error_response_body"
-	requiredErrorResponseBodyLocation = responseBodiesLocation + "required_error_response_body"
+    responseBodiesLocation                 = "./response_bodies/"
+    typeErrorResponseBodyLocation          = responseBodiesLocation + "type_error_response_body"
+    requiredErrorResponseBodyLocation      = responseBodiesLocation + "required_error_response_body"
 
-	statementEndpoint       = "/delta/psc-statement"
-	statementDeleteEndpoint = "/delta/psc-statement/delete"
-	apiSpecLocation         = "../../../apispec/api-spec.yml"
-	contextId               = "contextId"
-	methodPost              = "POST"
+    statementEndpoint       = "/delta/psc-statement"
+    statementDeleteEndpoint = "/delta/psc-statement/delete"
+    apiSpecLocation       = "../../../apispec/api-spec.yml"
+    contextId             = "contextId"
+    methodPost            = "POST"
 )
 
 // TestUnitPscStatementDeltaSchemaNoErrors asserts that when a valid request body is given which matches the schema, then no
 // errors are returned.
 func TestUnitPscStatementDeltaSchemaNoErrors(t *testing.T) {
 
-	Convey("Given I want to test the psc-statement-delta API schema", t, func() {
+    Convey("Given I want to test the psc-statement-delta API schema", t, func() {
 
-		okRequestBody := common.ReadRequestBody(okRequestBodyLocation)
+        okRequestBody := common.ReadRequestBody(okRequestBodyLocation)
 
-		r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(okRequestBody))
-		r = common.SetHeaders(r)
+        r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(okRequestBody))
+        r = common.SetHeaders(r)
 
-		Convey("When I call to validate the request body, providing a valid request", func() {
+        Convey("When I call to validate the request body, providing a valid request", func() {
 
-			chv, _ := validation.NewCHValidator(apiSpecLocation)
+            chv, _ := validation.NewCHValidator(apiSpecLocation)
 
-			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+            validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
 
-			Convey("Then I am given a nil response as no validation errors are returned", func() {
-				So(validationErrs, ShouldBeNil)
-			})
-		})
-	})
+            Convey("Then I am given a nil response as no validation errors are returned", func() {
+                So(validationErrs, ShouldBeNil)
+            })
+        })
+    })
 }
 
 // TestUnitPscStatementDeltaSchemaTypeErrors asserts that when an invalid request body is given with type errors (int provided
 // instead of string), then an errors array is returned.
 func TestUnitPscStatementDeltaSchemaTypeErrors(t *testing.T) {
 
-	Convey("Given I want to test the psc-statement-delta API schema for type assertions", t, func() {
+    Convey("Given I want to test the psc-statement-delta API schema for type assertions", t, func() {
 
-		typeErrorRequestBody := common.ReadRequestBody(typeErrorRequestBodyLocation)
+        typeErrorRequestBody := common.ReadRequestBody(typeErrorRequestBodyLocation)
 
-		r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(typeErrorRequestBody))
-		r = common.SetHeaders(r)
+        r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(typeErrorRequestBody))
+        r = common.SetHeaders(r)
 
-		Convey("When I call to validate the request body, providing an valid request with type errors", func() {
+        Convey("When I call to validate the request body, providing an valid request with type errors", func() {
 
-			chv, _ := validation.NewCHValidator(apiSpecLocation)
+            chv, _ := validation.NewCHValidator(apiSpecLocation)
 
-			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+            validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
 
-			Convey("Then I am given an errors array response as validation errors have been found", func() {
-				typeErrorResponseBody := common.ReadRequestBody(typeErrorResponseBodyLocation)
-				match := common.CompareActualToExpected(validationErrs, typeErrorResponseBody)
+            Convey("Then I am given an errors array response as validation errors have been found", func() {
+                typeErrorResponseBody := common.ReadRequestBody(typeErrorResponseBodyLocation)
+                match := common.CompareActualToExpected(validationErrs, typeErrorResponseBody)
 
-				So(validationErrs, ShouldNotBeNil)
-				So(match, ShouldEqual, true)
-			})
-		})
-	})
+                So(validationErrs, ShouldNotBeNil)
+                So(match, ShouldEqual, true)
+            })
+        })
+    })
 }
 
 // TestUnitPscStatementDeltaSchemaRequiredErrors asserts that when an invalid request body is given with missing mandatory values,
 // then an errors array is returned, stating that required values are missing.
 func TestUnitPscStatementDeltaSchemaRequiredErrors(t *testing.T) {
 
-	Convey("Given I want to test the psc-statement-delta API schema to assert mandatory validation is working correctly", t, func() {
+    Convey("Given I want to test the psc-statement-delta API schema to assert mandatory validation is working correctly", t, func() {
 
-		mandatoryErrorsRequestBody := common.ReadRequestBody(requiredErrorRequestBodyLocation)
+        mandatoryErrorsRequestBody := common.ReadRequestBody(requiredErrorRequestBodyLocation)
 
-		r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(mandatoryErrorsRequestBody))
-		r = common.SetHeaders(r)
+        r := httptest.NewRequest(methodPost, statementEndpoint, bytes.NewBuffer(mandatoryErrorsRequestBody))
+        r = common.SetHeaders(r)
 
-		Convey("When I call to validate the request body, providing an valid request with missing mandatory values", func() {
+        Convey("When I call to validate the request body, providing an valid request with missing mandatory values", func() {
 
-			chv, _ := validation.NewCHValidator(apiSpecLocation)
+            chv, _ := validation.NewCHValidator(apiSpecLocation)
 
-			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+            validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
 
-			Convey("Then I am given an errors array response as validation errors have been found", func() {
-				mandatoryErrorsResponseBody := common.ReadRequestBody(requiredErrorResponseBodyLocation)
-				match := common.CompareActualToExpected(validationErrs, mandatoryErrorsResponseBody)
+            Convey("Then I am given an errors array response as validation errors have been found", func() {
+                mandatoryErrorsResponseBody := common.ReadRequestBody(requiredErrorResponseBodyLocation)
+                match := common.CompareActualToExpected(validationErrs, mandatoryErrorsResponseBody)
 
-				So(validationErrs, ShouldNotBeNil)
-				So(match, ShouldEqual, true)
-			})
-		})
-	})
+                So(validationErrs, ShouldNotBeNil)
+                So(match, ShouldEqual, true)
+            })
+        })
+    })
 }
 
 // TestUnitPscStatementDeleteDeltaSchemaNoErrors asserts that when a valid request body is given which matches the schema, then no
@@ -113,23 +113,23 @@ func TestUnitPscStatementDeltaSchemaRequiredErrors(t *testing.T) {
 
 func TestUnitPscStatementDeleteDeltaSchemaNoErrors(t *testing.T) {
 
-	Convey("Given I want to test the psc-statement-delete-delta API schema", t, func() {
+    Convey("Given I want to test the psc-statement-delete-delta API schema", t, func() {
 
-		deleteRequestBody := common.ReadRequestBody(deleteRequestBodyLocation)
+        deleteRequestBody := common.ReadRequestBody(deleteRequestBodyLocation)
 
-		r := httptest.NewRequest(methodPost, statementDeleteEndpoint, bytes.NewBuffer(deleteRequestBody))
-		r = common.SetHeaders(r)
+        r := httptest.NewRequest(methodPost, statementDeleteEndpoint, bytes.NewBuffer(deleteRequestBody))
+        r = common.SetHeaders(r)
 
-		Convey("When I call to validate the request body, providing a valid request", func() {
+        Convey("When I call to validate the request body, providing a valid request", func() {
 
-			chv, _ := validation.NewCHValidator(apiSpecLocation)
+            chv, _ := validation.NewCHValidator(apiSpecLocation)
 
-			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+            validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
 
-			Convey("Then I am given a nil response as no validation errors are returned", func() {
-				So(validationErrs, ShouldBeNil)
-			})
-		})
-	})
+            Convey("Then I am given a nil response as no validation errors are returned", func() {
+                So(validationErrs, ShouldBeNil)
+            })
+        })
+    })
 }
 
