63d3819084672c3109458d385019fd135171f20c
kinpang-CH <kpang@companieshouse.gov.uk>
2024-02-19 15:00:09 +0000

    Add validation rules to filing history delta spec
    * Refactor ok valid request body json
    * Add new json snippets for all different validation scenarios
    * Add unit tests to test above scenarios

--- validation/schema_testing/filinghistory/filingHistoryDeltaSchema_test.go
+++ validation/schema_testing/filinghistory/filingHistoryDeltaSchema_test.go
@@ -10,8 +10,18 @@ import (
 )
 
 const (
-	requestBodiesLocation = "./request_bodies/"
-	okRequestBodyLocation = requestBodiesLocation + "valid_request.json"
+	requestBodiesLocation                = "./request_bodies/"
+	okRequestBody                        = requestBodiesLocation + "valid_request.json"
+	missingFieldsRequestBody             = requestBodiesLocation + "missing_required_fields_request.json"
+	missingFilingHistoryArrayRequestBody = requestBodiesLocation + "missing_filing_history_array_request.json"
+	invalidDataTypeRequestBody           = requestBodiesLocation + "invalid_data_type_request.json"
+	exceedsMaxLengthRequestBody          = requestBodiesLocation + "fields_exceeds_max_length_request.json"
+
+	responseBodiesLocation                = "./response_bodies/"
+	missingFieldsResponseBody             = responseBodiesLocation + "missing_required_fields_response.json"
+	missingFilingHistoryArrayResponseBody = responseBodiesLocation + "missing_filing_history_array_response.json"
+	invalidDataTypeResponseBody           = responseBodiesLocation + "invalid_data_type_response.json"
+	exceedsMaxLengthResponseBody          = responseBodiesLocation + "fields_exceeds_max_length_response.json"
 
 	filingHistoryEndpoint = "/delta/filing-history"
 	apiSpecLocation       = "../../../apispec/api-spec.yml"
@@ -20,22 +30,102 @@ const (
 )
 
 func TestUnitFilingHistoryDeltaSchemaNoErrors(t *testing.T) {
-
 	convey.Convey("Given a valid filing history delta request body has been specified", t, func() {
-
-		requestBody := common.ReadRequestBody(okRequestBodyLocation)
+		requestBody := common.ReadRequestBody(okRequestBody)
 
 		r := httptest.NewRequest(methodPost, filingHistoryEndpoint, bytes.NewBuffer(requestBody))
 		r = common.SetHeaders(r)
 
 		convey.Convey("When the request is validated", func() {
-
 			chv, _ := validation.NewCHValidator(apiSpecLocation)
-
 			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
 
 			convey.Convey("Then the returned value should be nil", func() {
 				convey.So(validationErrs, convey.ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestUnitFilingHistoryDeltaSchemaMissingRequiredFields(t *testing.T) {
+	convey.Convey("Given a filing history delta request body is missing all required fields", t, func() {
+		requestBody := common.ReadRequestBody(missingFieldsRequestBody)
+		r := httptest.NewRequest(methodPost, filingHistoryEndpoint, bytes.NewBuffer(requestBody))
+		r = common.SetHeaders(r)
+
+		convey.Convey("When the request is validated", func() {
+			chv, _ := validation.NewCHValidator(apiSpecLocation)
+			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+
+			convey.Convey("Then all validation errors should be returned", func() {
+				responseBody := common.ReadRequestBody(missingFieldsResponseBody)
+				match := common.CompareActualToExpected(validationErrs, responseBody)
+
+				convey.So(validationErrs, convey.ShouldNotBeNil)
+				convey.So(match, convey.ShouldBeTrue)
+			})
+		})
+	})
+}
+
+func TestUnitFilingHistoryDeltaSchemaMissingTopLevelField(t *testing.T) {
+	convey.Convey("Given a filing history delta request body is missing the top level filing history array", t, func() {
+		requestBody := common.ReadRequestBody(missingFilingHistoryArrayRequestBody)
+		r := httptest.NewRequest(methodPost, filingHistoryEndpoint, bytes.NewBuffer(requestBody))
+		r = common.SetHeaders(r)
+
+		convey.Convey("When the request is validated", func() {
+			chv, _ := validation.NewCHValidator(apiSpecLocation)
+			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+
+			convey.Convey("Then all validation errors should be returned", func() {
+				responseBody := common.ReadRequestBody(missingFilingHistoryArrayResponseBody)
+				match := common.CompareActualToExpected(validationErrs, responseBody)
+
+				convey.So(validationErrs, convey.ShouldNotBeNil)
+				convey.So(match, convey.ShouldBeTrue)
+			})
+		})
+	})
+}
+
+func TestUnitFilingHistoryDeltaSchemaWithInvalidDataType(t *testing.T) {
+	convey.Convey("Given a filing history delta request body contains fields which are int instead of string", t, func() {
+		requestBody := common.ReadRequestBody(invalidDataTypeRequestBody)
+		r := httptest.NewRequest(methodPost, filingHistoryEndpoint, bytes.NewBuffer(requestBody))
+		r = common.SetHeaders(r)
+
+		convey.Convey("When the request is validated", func() {
+			chv, _ := validation.NewCHValidator(apiSpecLocation)
+			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+
+			convey.Convey("Then all validation errors should be returned", func() {
+				responseBody := common.ReadRequestBody(invalidDataTypeResponseBody)
+				match := common.CompareActualToExpected(validationErrs, responseBody)
+
+				convey.So(validationErrs, convey.ShouldNotBeNil)
+				convey.So(match, convey.ShouldBeTrue)
+			})
+		})
+	})
+}
+
+func TestUnitFilingHistoryDeltaSchemaWhereFieldsExceedMaxLength(t *testing.T) {
+	convey.Convey("Given a filing history delta request body contains fields which exceeds its maximum length", t, func() {
+		requestBody := common.ReadRequestBody(exceedsMaxLengthRequestBody)
+		r := httptest.NewRequest(methodPost, filingHistoryEndpoint, bytes.NewBuffer(requestBody))
+		r = common.SetHeaders(r)
+
+		convey.Convey("When the request is validated", func() {
+			chv, _ := validation.NewCHValidator(apiSpecLocation)
+			validationErrs, _ := chv.ValidateRequestAgainstOpenApiSpec(r, contextId)
+
+			convey.Convey("Then all validation errors should be returned", func() {
+				responseBody := common.ReadRequestBody(exceedsMaxLengthResponseBody)
+				match := common.CompareActualToExpected(validationErrs, responseBody)
+
+				convey.So(validationErrs, convey.ShouldNotBeNil)
+				convey.So(match, convey.ShouldBeTrue)
 			})
 		})
 	})
