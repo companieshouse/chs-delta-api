95cd878ed5c5d384c68a3e1c4abc940305106699
sthompsonCH <105206260+sthompsonCH@users.noreply.github.com>
2024-10-09 12:11:26 +0100

    DSND-3109: Add delta_at to delete specs (#122)
    * DSND-3109: Add delta_at to delete specs
    
    * DSND-3109: Make delta_at optional for the time being to avoid breaking changes

--- config/keys.go
+++ config/keys.go
@@ -1,19 +1,19 @@
 package config
 
-//OpenApiSpecKey is the location where open api schema is stored
+// OpenApiSpecKey is the location where open api schema is stored
 const OpenApiSpecKey = "spec_location"
 
-//SchemaAbsolutePathKey is the key to get the absolute path to the schema
+// SchemaAbsolutePathKey is the key to get the absolute path to the schema
 const SchemaAbsolutePathKey = "schema_absolute_path"
 
-//TopicKey is the key to the name of the Kafka topic
+// TopicKey is the key to the name of the Kafka topic
 const TopicKey = "topic"
 
-//PartitionKey is the key to get the partition number of the topic
+// PartitionKey is the key to get the partition number of the topic
 const PartitionKey = "partition"
 
-//OffsetKey is the key to Offset of message from Kafka
+// OffsetKey is the key to Offset of message from Kafka
 const OffsetKey = "offset"
 
-//MessageKey is the key for the error message
+// MessageKey is the key for the error message
 const MessageKey = "message"
